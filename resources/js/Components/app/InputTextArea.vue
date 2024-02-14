e99327cbfc4a1ed0b359ba168b406b3b5edb27fe
Sahy <kakajansahy@gmail.com>
2024-02-15 00:07:03 +0200

    Implement Post update

--- resources/js/Components/app/InputTextArea.vue
+++ resources/js/Components/app/InputTextArea.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { onMounted, ref } from 'vue';
+// import {formatNodes} from "tailwindcss/src/oxide/cli/build/utils.js";
 
 const  props = defineProps({
     modelValue: {
@@ -34,12 +35,18 @@ defineExpose({ focus: () => input.value.focus() });
 
 function onInputChange($event){
     emit('update:modelValue', $event.target.value)
+    adjustHeight()
+}
+function adjustHeight(){
     if (props.autoResize){
         input.value.style.height = 'auto';
         input.value.style.height = input.value.scrollHeight + 'px';
     }
-
 }
+
+onMounted(()=>{
+    adjustHeight()
+})
 
 </script>
 
