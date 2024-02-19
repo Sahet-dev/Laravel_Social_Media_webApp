994e3bd2cd953a05659cfa4217caffae80e45e49
Sahy <kakajansahy@gmail.com>
2024-02-19 23:37:46 +0200

    Implement Attach files (frontend)

--- resources/js/Components/app/PostModal.vue
+++ resources/js/Components/app/PostModal.vue
@@ -1,74 +1,3 @@
-<template>
-    <teleport to="body">
-        <TransitionRoot appear :show="show" as="template">
-            <Dialog as="div" @close="closeModal" class="relative z-10">
-                <TransitionChild
-                    as="template"
-                    enter="duration-300 ease-out"
-                    enter-from="opacity-0"
-                    enter-to="opacity-100"
-                    leave="duration-200 ease-in"
-                    leave-from="opacity-100"
-                    leave-to="opacity-0"
-                >
-                    <div class="fixed inset-0 bg-black/25" />
-                </TransitionChild>
-
-                <div class="fixed inset-0 overflow-y-auto">
-                    <div
-                        class="flex min-h-full items-center justify-center p-4 text-center"
-                    >
-                        <TransitionChild
-                            as="template"
-                            enter="duration-300 ease-out"
-                            enter-from="opacity-0 scale-95"
-                            enter-to="opacity-100 scale-100"
-                            leave="duration-200 ease-in"
-                            leave-from="opacity-100 scale-100"
-                            leave-to="opacity-0 scale-95"
-                        >
-                            <DialogPanel
-                                class="w-full max-w-md transform overflow-hidden rounded bg-white
-                                text-left align-middle shadow-xl transition-all"
-                            >
-                                <DialogTitle
-                                    as="h3"
-                                    class="flex items-center justify-between py-3 px-4 font-medium bg-gray-100 text-gray-900"
-                                >
-                                    Update post
-                                    <button @click="show = false" class="w-8 h-8 rounded-full hover:bg-black/5 transition flex items-center
-                                        justify-center">
-                                        <XMarkIcon class="w-4 h-4"/>
-                                    </button>
-                                </DialogTitle>
-                                <div class="p-3 ">
-                                    <PostUserHeader :post="post" :show-time="false" class="mb-4"/>
-                                    <ckeditor :editor="editor" v-model="form.body" :config="editorConfig"></ckeditor>
-                                    <!--                                    <InputTextArea v-model="form.body" class="mb-3 w-full"/>-->
-                                </div>
-
-                                <div class="py-3 px-4">
-                                    <button
-                                        type="button"
-                                        class="rounded-md bg-indigo-600 px-3 py-2
-                                             text-sm font-semibold text-white
-                                            shadow-sm hover:bg-indigo-500 focus-visible:outline
-                                            focus-visible:outline-2 focus-visible:outline-offset-2
-                                            focus-visible:outline-indigo-600 w-full"
-                                        @click="submit"
-                                    >
-                                        Submit
-                                    </button>
-                                </div>
-                            </DialogPanel>
-                        </TransitionChild>
-                    </div>
-                </div>
-            </Dialog>
-        </TransitionRoot>
-    </teleport>
-</template>
-
 <script setup>
 import {computed, onMounted, reactive, ref, watch} from 'vue'
 import {
@@ -80,7 +9,7 @@ import {
 } from '@headlessui/vue'
 import InputTextArea from "@/Components/app/InputTextArea.vue";
 import PostUserHeader from "@/Components/app/PostUserHeader.vue";
-import { XMarkIcon } from '@heroicons/vue/24/solid'
+import { XMarkIcon, PaperClipIcon, BookmarkIcon } from '@heroicons/vue/24/solid'
 import {useForm} from "@inertiajs/vue3";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
@@ -134,5 +63,91 @@ function submit(){
     })
 }
 
-
+function onAttachmentChoose($event){
+    console.log(event.target.files)
+}
 </script>
+
+<template>
+    <teleport to="body">
+        <TransitionRoot appear :show="show" as="template">
+            <Dialog as="div" @close="closeModal" class="relative z-10">
+                <TransitionChild
+                    as="template"
+                    enter="duration-300 ease-out"
+                    enter-from="opacity-0"
+                    enter-to="opacity-100"
+                    leave="duration-200 ease-in"
+                    leave-from="opacity-100"
+                    leave-to="opacity-0"
+                >
+                    <div class="fixed inset-0 bg-black/25" />
+                </TransitionChild>
+
+                <div class="fixed inset-0 overflow-y-auto">
+                    <div
+                        class="flex min-h-full items-center justify-center p-4 text-center"
+                    >
+                        <TransitionChild
+                            as="template"
+                            enter="duration-300 ease-out"
+                            enter-from="opacity-0 scale-95"
+                            enter-to="opacity-100 scale-100"
+                            leave="duration-200 ease-in"
+                            leave-from="opacity-100 scale-100"
+                            leave-to="opacity-0 scale-95"
+                        >
+                            <DialogPanel
+                                class="w-full max-w-md transform overflow-hidden rounded bg-white
+                                text-left align-middle shadow-xl transition-all"
+                            >
+                                <DialogTitle
+                                    as="h3"
+                                    class="flex items-center justify-between py-3 px-4 font-medium bg-gray-100 text-gray-900"
+                                >
+                                    Update post
+                                    <button @click="show = false" class="w-8 h-8 rounded-full hover:bg-black/5 transition flex items-center
+                                        justify-center">
+                                        <XMarkIcon class="w-4 h-4"/>
+                                    </button>
+                                </DialogTitle>
+                                <div class="p-3 ">
+                                    <PostUserHeader :post="post" :show-time="false" class="mb-4"/>
+                                    <ckeditor :editor="editor" v-model="form.body" :config="editorConfig"></ckeditor>
+                                    <!--                                    <InputTextArea v-model="form.body" class="mb-3 w-full"/>-->
+                                </div>
+
+                                <div class="flex gap-2 py-3 px-4">
+                                    <button
+                                        type="button"
+                                        class="flex items-center justify-center rounded-md bg-indigo-600 px-3 py-2
+                                             text-sm font-semibold text-white
+                                            shadow-sm hover:bg-indigo-500 focus-visible:outline
+                                            focus-visible:outline-2 focus-visible:outline-offset-2
+                                            focus-visible:outline-indigo-600 w-full relative"
+                                        @click="submit"
+                                    >
+                                        <PaperClipIcon class="w-4 h-4 mr-2"/>Attach Files
+                                        <input @click.stop @change="onAttachmentChoose" type="file" multiple class="absolute left-0 top-0 right-0 bottom-0 opacity-0">
+                                    </button>
+                                    <button
+                                        type="button"
+                                        class="flex items-center justify-center rounded-md bg-indigo-600 px-3 py-2
+                                             text-sm font-semibold text-white
+                                            shadow-sm hover:bg-indigo-500 focus-visible:outline
+                                            focus-visible:outline-2 focus-visible:outline-offset-2
+                                            focus-visible:outline-indigo-600 w-full"
+                                        @click="submit"
+                                    >
+                                        <BookmarkIcon class="w-4 h-4 mr-2"/>Submit
+                                    </button>
+                                </div>
+                            </DialogPanel>
+                        </TransitionChild>
+                    </div>
+                </div>
+            </Dialog>
+        </TransitionRoot>
+    </teleport>
+</template>
+
