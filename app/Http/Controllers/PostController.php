f8e484ce0f5c19446228ec2c47eff8e7747e6ced
Sahy <kakajansahy@gmail.com>
2024-02-23 10:56:09 +0200

    Improvements. Implement Attach files (Backend)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,8 +5,13 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
 use App\Models\Post;
+use App\Models\PostAttachment;
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -17,7 +22,37 @@ class PostController extends Controller
     public function store(StorePostRequest $storePostRequest)
     {
         $data = $storePostRequest->validated();
-        Post::create($data);
+        $user = $storePostRequest->user();
+
+        DB::beginTransaction();
+        $allFilePaths = [];
+        try {
+            $post = Post::create($data);
+            /** @var \Illuminate\Http\UploadedFile[] $files */
+            $files = $data['attachments'] ?? [];
+            foreach ($files as $file){
+                $path = $file->store('attachments/'.$post->id, 'public');
+                $allFilePaths[] = $path;
+                PostAttachment::create([
+                    'post_id' => $post->id,
+                    'name' => $file->getClientOriginalName(),
+                    'path' => $path,
+                    'mime' => $file->getMimeType(),
+                    'size' => $file->getSize(),
+                    'created_by' => $user->id,
+
+                ]);
+            }
+            DB::commit();
+        } catch (\Exception $exception){
+            foreach ($allFilePaths as $path){
+                Storage::disk('public')->delete($path);
+            }
+
+            DB::rollBack();
+            throw $exception;
+        }
+
         return back();
     }
 
--- resources/js/Components/app/PostModal.vue
+++ resources/js/Components/app/PostModal.vue
@@ -39,7 +39,8 @@ const attachhmentFiles = ref([]);
 
 const form = useForm({
     id: null,
-    body: ''
+    body: '',
+    attachments: []
 })
 
 const reactivePost = reactive(props.post)
@@ -60,16 +61,32 @@ watch(()=> props.post, ()=>{
 
 function closeModal() {
     show.value = false
+    resetModal()
     // emit('update:modalValue', false)
 }
-function submit(){
 
-    form.put(route('post.update', props.post.id),{
-        preserveScroll: true,
-        onSuccess: ()=>{
-            show.value = false;
-        }
-    })
+function resetModal(){
+    form.reset()
+    attachhmentFiles.value = []
+}
+
+function submit(){
+    form.attachments = attachhmentFiles.value.map(myFile => myFile.file)
+    if (form.id){
+        form.put(route('post.update', props.post.id),{
+            preserveScroll: true,
+            onSuccess: ()=>{
+                closeModal()
+            }
+        })
+    }else {
+        form.post(route('post.create'),{
+            preserveScroll: true,
+            onSuccess: ()=>{
+                closeModal()
+            }
+        })
+    }
 }
 
 async function onAttachmentChoose($event) {
@@ -154,11 +171,11 @@ function removeFile(myFile){
                                     <PostUserHeader :post="post" :show-time="false" class="mb-4"/>
                                     <ckeditor :editor="editor" v-model="form.body" :config="editorConfig"></ckeditor>
                                     <div class="grid grid-cols-2 lg:grid-cols-3 gap-3 my-3 ">
-                                        <template v-for="myFile of attachhmentFiles" class="">
+                                        <template v-for="(myFile, ind) of attachhmentFiles" class="">
                                             <div class="group aspect-square items-center justify-center bg-blue-100
                                                 flex flex-col items-center justify-center text-gray-500 relative">
                                                 <button @click="removeFile(myFile)" class="absolute right-3 top-3 w-7 h-7 flex items-center
-                                                justify-center bg-black/20
+                                                justify-center bg-black/30
                                                     text-white rounded-full hover:bg-gray-400">
                                                     <XMarkIcon class="w-5 h-5 "/>
 
@@ -167,7 +184,9 @@ function removeFile(myFile){
                                                      class="object-cover aspect-square ">
                                                 <template v-else>
                                                    <PaperClipIcon class="w-12 h-12 mb-3"/>
-                                                    <small class="text-center ">{{myFile.file.name}}</small>
+                                                    <small  class="text-center ">
+                                                        {{myFile.file.name}}
+                                                    </small>
                                                 </template>
                                             </div>
                                         </template>
